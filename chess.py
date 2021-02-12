d8bd90cc1a6067d7586fe7c56c473ea563152b19
Adam Kern <adam.kern@wustl.edu>
2021-02-12 17:18:24 -0500

    Drawing board

--- chess.py
+++ chess.py
@@ -5,6 +5,7 @@ import arcade
 # Constants
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 750
+WIDTH_BUFFER = SCREEN_WIDTH - SCREEN_HEIGHT
 SCREEN_TITLE = "Chess"
 
 
@@ -24,6 +25,36 @@ class ChessGame(arcade.Window):
     def on_draw(self):
         """Render the screen."""
         arcade.start_render()
+        draw_board()
+
+
+def draw_board():
+    arcade.draw_lrtb_rectangle_outline(
+        0,
+        SCREEN_WIDTH - WIDTH_BUFFER,
+        SCREEN_HEIGHT,
+        0,
+        arcade.csscolor.BLACK,
+        border_width=10,
+    )
+    SQUARE_SIZE = SCREEN_HEIGHT / 8
+    color_white = False
+    for row in range(8):
+        for col in range(8):
+            color = (
+                arcade.csscolor.IVORY
+                if color_white
+                else arcade.csscolor.DARK_SLATE_GRAY
+            )
+            arcade.draw_lrtb_rectangle_filled(
+                col * SQUARE_SIZE,
+                (col + 1) * SQUARE_SIZE,
+                (row + 1) * SQUARE_SIZE,
+                row * SQUARE_SIZE,
+                color,
+            )
+            color_white = not color_white
+        color_white = not color_white
 
 
 def main():
